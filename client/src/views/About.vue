04ecabe90db9d227c00bcaec9d0961e9e9f8d1bb
Floribunda1 <1094083688@qq.com>
2021-05-07 15:20:01 +0800

    revert: 重新使用 webpack

---
+++ client/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1 @click="handleClick">This is an about page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  setup() {
+    const handleClick = (e: MouseEvent) => {
+      console.log(e);
+    };
+    return { handleClick };
+  },
+});
+</script>
